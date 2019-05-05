cc1cb60cd30e99e64b98e48ac8925fcc318dfd4f
markstepchin <markstepchin@gmail.com>
2019-05-04 18:15:21 -0700

    remove unnecessary export

--- src/Layout/Footers.js
+++ src/Layout/Footers.js
@@ -2,13 +2,6 @@ import React from "react";
 import { Link } from "react-router-dom";
 import SignOutButton from "../components/SignOutButton";
 
-export const FooterLayout = ({children}) => (
-  <div className="footer-layout">
-    {children}
-    <span className="copyrite">&copy; Mark Stepchin, 2019</span>
-  </div>
-);
-
 export const EmptyFooter = () => <FooterLayout />;
 
 export const LandingPageFooter = () => 
@@ -26,3 +19,9 @@ export const SignOutFooter = () =>
     <SignOutButton />
   </FooterLayout>;
   
+const FooterLayout = ({children}) => (
+  <div className="footer-layout">
+    {children}
+    <span className="copyrite">&copy; Mark Stepchin, 2019</span>
+  </div>
+);
