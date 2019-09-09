fb4dde8cd2ceda62147dfb60b11aded37114f7a7
markstepchin <markstepchin@gmail.com>
2019-09-09 10:43:59 -0700

    Style the address navigation link

--- src/Layout/Details.js
+++ src/Layout/Details.js
@@ -16,18 +16,6 @@ const Details = () => (
     <div className="details-section">
       <div>
         <div className="detail-icons">
-          <i className="far fa-calendar-alt" />
-        </div>
-        <span>
-          {startDate.format('MMMM')} {startDate.date()}-{endDate.date()}
-        </span>
-        <br />
-        <a href="">Add to Calendar</a>
-      </div>
-    </div>
-    <div className="details-section">
-      <div>
-        <div className="detail-icons">
           <i className="fas fa-map-marker-alt" />
         </div>
         <span>Waitts Lake Open Bible Camp</span>
@@ -36,25 +24,37 @@ const Details = () => (
         <br />
         <span>Valley, WA 99181</span>
         <br />
-        <a
-          href="https://www.google.com/maps/place/Waitts+Lake+Open+Bible+Camp/@48.2018545,-117.7937229,17z/data=!3m1!4b1!4m5!3m4!1s0x549df5702f21f67b:0xb41c3589b0070ad5!8m2!3d48.2018509!4d-117.7915342"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Navigate
-        </a>
+        <div className="address-link-container">
+          <a
+            href="https://www.google.com/maps/place/Waitts+Lake+Open+Bible+Camp/@48.2018545,-117.7937229,17z/data=!3m1!4b1!4m5!3m4!1s0x549df5702f21f67b:0xb41c3589b0070ad5!8m2!3d48.2018509!4d-117.7915342"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="address-link"
+          >
+            <span style={{ marginRight: '.5rem' }}>Navigate</span>
+            <i className="fas fa-car-alt" />
+          </a>
+        </div>
       </div>
     </div>
     <div className="details-section">
       <div>
         <div className="detail-icons">
+          <i className="far fa-calendar-alt" />
+        </div>
+        <div style={{ textAlign: 'center' }}>
+          {startDate.format('MMMM')} {startDate.date()}-{endDate.date()}
+        </div>
+      </div>
+    </div>
+    <div className="details-section">
+      <div style={{ textAlign: 'center' }}>
+        <div className="detail-icons">
           <i className="fas fa-money-check-alt" />
         </div>
-        <span>Total: ${calcCost(START_DATE, END_DATE)}</span>
+        <span style={{ textAlign: 'center' }}>Total: ${calcCost(START_DATE, END_DATE)}</span>
         <br />
-        <span>
-          <i>(or ${COST_PER_DAY} per day)</i>
-        </span>
+        <i>(or ${COST_PER_DAY} per day)</i>
       </div>
     </div>
   </>
