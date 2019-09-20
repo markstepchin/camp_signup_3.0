593870e4a24f23f9204108ebf0961077c74fc3e6
markstepchin <markstepchin@gmail.com>
2019-09-20 16:50:55 -0700

    Update the dates

--- src/constants/CampDetails.js
+++ src/constants/CampDetails.js
@@ -2,5 +2,5 @@ export const NAME = 'Spokamp 2019';
 export const HEADING = 'Most fun camp of the year!';
 export const DESCRIPTION = 'Short description about how amazing this camp will be...';
 
-export const START_DATE = '2018-07-01';
-export const END_DATE = '2018-07-05';
+export const START_DATE = '2019-10-31';
+export const END_DATE = '2019-11-03';
--- src/constants/DateOptions.js
+++ src/constants/DateOptions.js
@@ -1,23 +1,19 @@
 const dateOptions = [
   {
-    value: '2018-07-01',
-    display: 'July 1',
+    value: '2019-10-31',
+    display: 'Oct 31',
   },
   {
-    value: '2018-07-02',
-    display: 'July 2',
+    value: '2019-11-01',
+    display: 'Nov 1',
   },
   {
-    value: '2018-07-03',
-    display: 'July 3',
+    value: '2019-11-02',
+    display: 'Nov 2',
   },
   {
-    value: '2018-07-04',
-    display: 'July 4',
-  },
-  {
-    value: '2018-07-05',
-    display: 'July 5',
+    value: '2019-11-03',
+    display: 'Nov 3',
   },
 ];
 
